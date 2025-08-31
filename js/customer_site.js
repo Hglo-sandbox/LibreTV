7ed74a84ddc7740b32df5c7a6902b4d714405c00
heguole <heguole@heguoledeMacBook-Pro.local>
2025-08-31 12:00:58 +0800

    updated sources

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,7 +1,27 @@
 const CUSTOMER_SITES = {
-    qiqi: {
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七七资源',
+    apibdzy: {
+        api: 'https://api.apibdzy.com/api.php/provide/vod',
+        name: '百度云资源',
+    },
+    ukuapi: {
+        api: 'https://api.ukuapi.com/api.php/provide/vod',
+        name: 'U酷资源',
+    },
+    wujinapi: {
+        api: 'https://api.wujinapi.com/api.php/provide/vod',
+        name: '无尽资源',
+    },
+    dbzy: {
+        api: 'https://dbzy.tv/api.php/provide/vod',
+        name: '豆瓣资源',
+    },
+    apibdzy: {
+        api: 'https://api.apibdzy.com/api.php/provide/vod﻿',
+        name: '百度资源',
+    },
+    ukuapi88: {
+        api: 'https://api.ukuapi88.com/api.php/provide/vod',
+        name: '优酷资源',
     }
 };
 
